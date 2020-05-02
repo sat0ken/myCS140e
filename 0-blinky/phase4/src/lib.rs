d4f7063c58ac5eb62a5a05247c0ba57effa17e86
sat0ken <15720506+sat0ken@users.noreply.github.com>
2020-05-02 11:48:17 +0900

    add files

---
+++ 0-blinky/phase4/src/lib.rs
@@ -0,0 +1,35 @@
+#![feature(compiler_builtins, lang_items, asm, pointer_methods)]
+#![no_builtins]
+#![no_std]
+#![feature(rustc_private)]
+
+extern crate compiler_builtins as other_compiler_builtins;
+extern crate panic_halt;
+
+pub mod lang_items;
+
+const GPIO_BASE: usize = 0x3F000000 + 0x200000;
+
+const GPIO_FSEL1: *mut u32 = (GPIO_BASE + 0x04) as *mut u32;
+const GPIO_SET0: *mut u32 = (GPIO_BASE + 0x1C) as *mut u32;
+const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
+
+#[inline(never)]
+fn spin_sleep_ms(ms: usize) {
+    for _ in 0..(ms * 6000) {
+        unsafe { asm!("nop" :::: "volatile"); }
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn kmain() {
+    // FIXME: STEP 1: Set GPIO Pin 16 as output.
+    GPIO_FSEL1.write_volatile(0b001 << 18);
+    // FIXME: STEP 2: Continuously set and clear GPIO 16.
+    loop {
+        GPIO_SET0.write_volatile(0x001 << 16);
+        spin_sleep_ms(1000);
+        GPIO_CLR0.write_volatile(0x001 << 16);
+        spin_sleep_ms(1000);
+    }
+}
